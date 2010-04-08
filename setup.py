298b0751dc414eb468b1d26bbd820e1b0c82b305
Micahel Carter <cartermichael@gmail.com>
2010-04-08 14:00:49 +0000

    Release 0.1alpha7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os, sys
 
 setup(
     name='csp',
-    version='0.1alpha6',
+    version='0.1alpha7',
     author='Michael Carter',
     author_email='CarterMichael@gmail.com',
     url='http://www.orbited.org',
